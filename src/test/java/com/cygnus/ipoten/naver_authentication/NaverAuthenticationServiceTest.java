271185ae19667bc23ab0338656a579f4c44aee0e
Hyunsoo Choi <gustn9025@naver.com>
2025-11-12 17:13:09 +0900

    test: [최현수] 네이버 소셜 로그인 링크 요청 테스트 코드 추가

---
+++ src/test/java/com/cygnus/ipoten/naver_authentication/NaverAuthenticationServiceTest.java
@@ -0,0 +1,86 @@
+package com.cygnus.ipoten.naver_authentication;
+
+import com.cygnus.ipoten.account.entity.LoginType;
+import com.cygnus.ipoten.accountProfile.service.AccountProfileService;
+import com.cygnus.ipoten.authentication.service.AuthenticationService;
+import com.cygnus.ipoten.authentication.service.AuthenticationServiceImpl;
+import com.cygnus.ipoten.config.FrontendConfig;
+import com.cygnus.ipoten.kakao_authentication.service.KakaoAuthenticationServiceImpl;
+import com.cygnus.ipoten.naver_authentication.service.NaverAuthenticationService;
+import com.cygnus.ipoten.naver_authentication.service.NaverAuthenticationServiceImpl;
+import com.cygnus.ipoten.naver_authentication.service.response.NaverLoginResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class NaverAuthenticationServiceTest {
+
+    private NaverAuthenticationService naverAuthenticationService;
+
+    @Mock
+    private FrontendConfig frontendConfig;
+    @Mock
+    private AuthenticationService authService;
+    @Mock
+    private AccountProfileService accountProfileService;
+    @Mock
+    private AuthenticationServiceImpl authenticationService;
+    @Mock
+    private RestTemplate restTemplate;
+
+
+    private String loginUrl = "https://oauth.naver.com";
+    private String clientId = "test-client-id";
+    private String clientSecret = "test-client-secret";
+    private String redirectUri = "http://localhost:8080/authentication/naver/login";
+
+    @BeforeEach
+    public void setUp() {
+        naverAuthenticationService = new NaverAuthenticationServiceImpl(
+                loginUrl,
+                clientId,
+                clientSecret,
+                redirectUri,
+                restTemplate,
+                frontendConfig,
+                authService,
+                accountProfileService
+
+        ) {
+        };
+    }
+
+
+    @Test
+    @DisplayName("네이버_소셜_로그인_링크를_전달_받습니다")
+    void 네이버_소셜_로그인_링크를_전달_받습니다() {
+
+        // given
+        // when
+
+        String naverOAuthLink = naverAuthenticationService.link();
+
+        String naverLink = String.format(
+                "%s?client_id=%s&response_type=code&redirect_uri=%s&state=RANDOM_STRING",
+                loginUrl, clientId, redirectUri
+        );
+
+        // then
+
+        Assertions.assertEquals(naverOAuthLink, naverLink);
+
+    }
+
+    
+}
