b878dd4d87f99cc1833ff4f52d42cc3c45a98412
Hyunsoo Choi <gustn9025@naver.com>
2025-11-14 13:33:26 +0900

    feat: [최현수] 네이버 소셜 로그인 시 신규 회원일 경우 임시 토큰을 통해 약관 동의를 진행합니다 [IPB-6]

--- src/main/java/com/cygnus/ipoten/naver_authentication/controller/NaverAuthenticationController.java
+++ src/main/java/com/cygnus/ipoten/naver_authentication/controller/NaverAuthenticationController.java
@@ -2,9 +2,14 @@ package com.cygnus.ipoten.naver_authentication.controller;
 
 
 import com.cygnus.ipoten.naver_authentication.service.NaverAuthenticationService;
+import com.cygnus.ipoten.naver_authentication.service.response.NaverLoginResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -22,8 +27,32 @@ public class NaverAuthenticationController {
     }
 
     @GetMapping("/login")
-    public String login(@RequestParam String code){
-        log.info("login code: {}", code);
-        return "테스트 성공";
+    public void login(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
+
+        try {
+            NaverLoginResponse naverLoginResponse = naverAuthenticationService.handleLogin(code);
+            if(!naverLoginResponse.getIsNewUser()){
+                String cookieHeader = String.format(
+                        "userToken=%s; Max-Age=%d; Path=/; HttpOnly; Secure; SameSite=Strict",
+                        naverLoginResponse.getUserToken(),
+//                    12 * 60 * 60
+                        6 * 60 * 60// 6시간
+
+                );        // CSRF 방어
+                response.addHeader("Set-Cookie", cookieHeader);
+            }
+
+            response.setContentType("text/html;charset=UTF-8");
+            response.getWriter().write(naverLoginResponse.getHtmlResponse());
+
+        }catch (Exception e){
+            log.error("Meta 로그인 에러", e);
+
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.setContentType("text/html;charset=UTF-8");
+            response.getWriter().write("Meta 로그인 실패: " + e.getMessage());
+
+        }
+
     }
 }
