19f0d280b997f63a3e53f6e9c83c804ddfde7930
Hyunsoo Choi <gustn9025@naver.com>
2026-02-10 17:08:40 +0900

    feat: [최현수] 인증된 사용자는 I-Poten 서비스의 리뷰를 남길 수 있습니다 [IPB-54]

---
+++ src/main/java/com/cygnus/ipoten/ipoten_review/repository/IpotenReviewRepository.java
@@ -0,0 +1,8 @@
+package com.cygnus.ipoten.ipoten_review.repository;
+
+import com.cygnus.ipoten.ipoten_review.entity.IpotenReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IpotenReviewRepository extends JpaRepository<IpotenReview, Long> {
+
+}
