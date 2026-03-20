73bcf2a1fcab43faf526965690c0d1f94acf5e06
min <wittgen0024@gmail.com>
2026-03-20 15:53:26 +0900

    feat: [김정민] 일정(Schedule) 도메인 전체 구현

---
+++ src/main/java/com/cygnus/ipoten/schedule/service/request/CreateScheduleRequest.java
@@ -0,0 +1,31 @@
+package com.cygnus.ipoten.schedule.service.request;
+
+import com.cygnus.ipoten.account.entity.Account;
+import com.cygnus.ipoten.schedule.entity.Schedule;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
+
+@Getter
+@RequiredArgsConstructor
+public class CreateScheduleRequest {
+
+    private final Long accountId;
+    private final String title;
+    private final String memo;
+    private final Instant startAt;
+    private final Instant endAt;
+    private final boolean allDay;
+
+    public Schedule toSchedule() {
+        return new Schedule(
+                new Account(accountId),
+                title.trim(),
+                memo.trim(),
+                startAt,
+                endAt,
+                allDay
+        );
+    }
+}
