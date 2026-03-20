04061f831f2d4d8fa1ca16dae0b4888b826280e8
min <wittgen0024@gmail.com>
2026-03-20 10:40:06 +0900

    feat: [김정민] 문의(Inquiry) 도메인 전체 구현

---
+++ src/main/java/com/cygnus/ipoten/inquiry/entity/InquiryType.java
@@ -0,0 +1,9 @@
+package com.cygnus.ipoten.inquiry.entity;
+
+public enum InquiryType {
+    SERVICE,    // 서비스 문의
+    PAYMENT,    // 결제 문의
+    BUG,        // 오류 제보
+    FEATURE,    // 기능 제안
+    OTHER       // 기타
+}
